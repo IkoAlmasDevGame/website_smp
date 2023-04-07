188557bada8485ec19b82538428d40264c8fa073
IkoAlmasDevGame <ikoalmas27@gmail.com>
2023-04-07 09:05:21 +0700

    Master

--- app/config/bootstrap.php
+++ app/config/bootstrap.php
@@ -2,26 +2,18 @@
 include "../config/config.php";
 ?>
 
-<!-- Link CSS Bootstrap Modified -->
-<link rel="stylesheet" href="<?=base()?>system/css/bootstrapv5221.css" type="text/css">
-<link rel="stylesheet" href="<?=base()?>system/css/card-bootstrap.css" type="text/css">
-<link rel="stylesheet" href="<?=base()?>system/css/font-awesome4.min.css" type="text/css">
-<link rel="stylesheet" href="<?=base()?>system/css/fontawesome.css" type="text/css">
-<link rel="stylesheet" href="<?=base()?>system/css/text-bootstrap.css" type="text/css">
-<link rel="stylesheet" href="<?=base()?>system/css/glyphicon.css" type="text/css">
-<link rel="stylesheet" href="<?=base()?>system/css/all.css" type="text/css">
+<link rel="stylesheet" href="<?=base()?>system/css/bootstrapv5221.css">
+<link rel="stylesheet" href="<?=base()?>system/css/tailwind.css">
+<link rel="stylesheet" href="<?=base()?>system/css/text-bootstrap.css">
+<link rel="stylesheet" href="<?=base()?>system/css/card-bootstrap.css">
+<link rel="stylesheet" href="<?=base()?>system/css/font-awesome4.min.css">
+<link rel="stylesheet" href="<?=base()?>system/css/fontawesome.css">
+<link rel="stylesheet" href="<?=base()?>system/css/glyphicon.css">
 
-<!-- Link CSS Tailwind -->
-<link rel="stylesheet" href="<?=base()?>stystem/dist/tailwind.css" type="text/css">
-<!-- Link Icon Image -->
+<link rel="stylesheet" href="<?=base()?>app/css/Home/style.css">
 <link rel="shortcut icon" href="<?=base()?>assets/icon/smp.ico" type="image/x-icon">
-<!-- Link CSS -->
-<link rel="stylesheet" href="<?=base()?>app/css/Home/style.css" type="text/css">
 
-<!-- Script Bootstrap Js -->
-<script src="<?=base()?>system/js/bootstrap.bundle.min.js" type="text/javascript"></script>
-<script src="<?=base()?>system/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="<?=base()?>system/js/bootstrap.js" type="text/javascript"></script>
-<script src="<?=base()?>system/js/fontawesome.js" type="text/javascript"></script>
-<script src="<?=base()?>system/js/all.js" type="text/javascript"></script>
-
+<script src="<?=base()?>system/js/bootstrap.min.js"></script>
+<script src="<?=base()?>system/js/bootstrap.js"></script>
+<script src="<?=base()?>system/js/bootstrap.bundle.js"></script>
+<script src="<?=base()?>system/js/fontawesome.js"></script>
